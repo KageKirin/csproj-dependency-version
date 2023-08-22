813640602469c03976237a50fe4acbd5a41064e7
Chris Helmich <kagekirin@gmail.com>
2023-08-22 09:07:06 +0900

    refactor: move try-catch block inside foreach-loop in bump_version()

--- index.js
+++ index.js
@@ -192,9 +192,9 @@ function set_version()
 
 function bump_version()
 {
-    try
-    {
-        args.files.forEach((file) => {
+    args.files.forEach((file) => {
+        try
+        {
             const doc = read_csproj(file);
             const verAttribute = get_csproj_package_version(doc);
             if (verAttribute)
@@ -274,13 +274,13 @@ function bump_version()
                 console.error("invalid .csproj does not contain package reference version at read back");
                 return 1;
             }
-        })
-    }
-    catch (error)
-    {
-        console.error(error.message);
-        return 1;
-    }
+        }
+        catch (error)
+        {
+            console.error(error.message);
+            return 1;
+        }
+    })
 
     return 0;
 }
